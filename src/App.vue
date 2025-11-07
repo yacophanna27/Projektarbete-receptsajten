898098a71447680b5436477eaae2a9a6d5144094
Louise Rylander <florinellas@gmail.com>
2025-11-07 12:48:03 +0100

    added links to font and footer

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@ import RecipeCard from './components/RecipeCard.vue';
 import { fetchRecipes } from './fetchRecipes.js';
 import NavBar from './components/NavBar.vue';
 import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   components: {
     RecipeCard,
     NavBar,
-    Header
+    Header,
+    Footer
   },
   data() {
     return {
@@ -22,6 +24,7 @@ export default {
   <NavBar />
   <Header />
   <RecipeCard v-for="(recipe, index) in recipes" :key="index" :recipe="recipe" />
+  <Footer />
 
 </template>
 
---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <footer class="footer">
+        <p>© 2025 New Year's Eve Recipes. All rights reserved.</p>
+    </footer>
+</template>
+
+<style scoped>
+footer {
+    font-family:'montserrat', sans-serif;
+    background-color: var(--color-dark);
+     color: var(--color-gold);
+    text-align: center;
+    padding: 1rem 0;
+    margin-top: 2rem;
+    font-size: 1rem;
+}
+</style>
