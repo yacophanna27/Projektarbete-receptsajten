a6a84f86e8c9545e49cc49cef31e2bf389a7c16d
yacophanna27 <yacophanna27@gmail.com>
2025-11-07 19:39:03 +0100

    Category list layout (not functional yet)

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@ import RecipeCard from './components/RecipeCard.vue';
 import { fetchRecipes } from './fetchRecipes.js';
 import NavBar from './components/NavBar.vue';
 import Header from './components/Header.vue';
+import Categories from './components/Categories.vue';
 
 export default {
   components: {
     RecipeCard,
     NavBar,
-    Header
+    Header,
+    Categories
   },
   data() {
     return {
@@ -21,6 +23,7 @@ export default {
 <template>
   <NavBar />
   <Header />
+  <Categories />
   <RecipeCard v-for="(recipe, index) in recipes" :key="index" :recipe="recipe" />
 
 </template>
---
+++ src/components/Categories.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+  props: [ ],
+  data() {
+    return {
+
+      // Define any component-specific data properties here
+    };
+  },
+  methods: {
+    // Define any component-specific methods here
+}
+}
+</script>
+
+<template>
+    <ul class=" categories">
+        <li>All</li>
+        <li>Starters</li>
+        <li>Main Courses</li>
+        <li>Desserts</li>
+        <li>Drinks</li>
+
+    </ul>
+</template>
+
+
+<style scoped>  
+.categories {
+  display: flex;
+  flex-direction: row;
+  font-size: 1rem;
+  justify-content: center;
+  align-items: center;
+  list-style: none;
+  margin-top: 4rem;
+ 
+}
+
+.categories li {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(133, 99, 178, 0.5);
+  backdrop-filter: blur(20px);
+  padding: 1.2rem 2.5rem;
+  margin-bottom: 1.5rem;
+  margin-inline: 1rem;
+  border-radius: 2rem;
+  font-size: 1rem;
+  font-family: Arial, Helvetica, sans-serif;
+  color: white;
+
+}
+
+</style>
