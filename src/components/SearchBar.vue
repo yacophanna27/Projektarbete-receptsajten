873004f0e23d570cfff7e45504ad76e0c44deb27
Louise Rylander <florinellas@gmail.com>
2025-11-09 12:15:27 +0100

    added Searchbar.vue code in progress

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<template>
+
+    <div class="search-bar">
+        <input type="text" placeholder="Search for recipes..." />
+        <button type="submit">Search</button>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
