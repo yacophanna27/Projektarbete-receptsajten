a8808eb4fce3c8c4dad2d733e90906bd242c8a70
Louise Rylander <florinellas@gmail.com>
2025-11-11 14:45:32 +0100

    deleted search categories and changed color

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,16 +1,11 @@
-/<template>
+<template>
   <div class="search-bar">
     <input
       type="text"
       v-model="searchText"
       placeholder="Sök recept..."
+      @keyup.enter="search"
     />
-
-    <select v-model="selectedCategory">
-      <option value="">Alla kategorier</option>
-      <option v-for="cat in categories" :key="cat">{{ cat }}</option>
-    </select>
-
     <button @click="search">Sök</button>
   </div>
 </template>
@@ -20,15 +15,13 @@ export default {
   name: 'SearchBar',
   data() {
     return {
-      searchText: '',
-      selectedCategory: '',
-      categories: ['Förrätt', 'Varmrätt', 'Dessert', 'Drinkar']
+      searchText: ''
     }
   },
   methods: {
     search() {
-      console.log('Söker efter:', this.searchText, 'i kategori:', this.selectedCategory)
-      
+      console.log('Söker efter:', this.searchText)
+      // recepten
     }
   }
 }
@@ -40,15 +33,15 @@ export default {
   display: flex;
   align-items: center;
   gap: 0.5rem;
-  color: gold
+  color:#b39108;
 }
 
-input, select {
+input {
   padding: 0.4rem 0.6rem;
   border-radius: 8px;
   border: 2px solid #582f62;
   background-color: var(--light-purple);
-  color: gold;
+  color: #b39108;
 }
 
 button {
@@ -57,12 +50,10 @@ button {
   background-color: var(--dark-purple);
   border: none;
   border-radius: 8px;
-  color: gold;
+  color: #b39108;
   cursor: pointer;
   font-weight: bold;
-  font-family: 'montserrat', sans-serif;
 }
-
 
 button:hover {
   opacity: 0.9;
