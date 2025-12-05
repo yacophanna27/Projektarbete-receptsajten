71d9e799c27553bd554db7a709a194074742b207
rusanovasnataly <rusanovasnataly@gmail.com>
2025-12-05 12:18:46 +0100

    Created ListComponent and made changes on RecipeView

--- src/Views/RecipeView.vue
+++ src/Views/RecipeView.vue
@@ -1,6 +1,7 @@
 <script>
 import { getAllRecipes, getRecipeById } from '../APIutilities/apihelpers.js';
 import Ratingstars from '../components/Ratingstars.vue';
+import ListComponent from '../components/ListComponent.vue';
 
 export default {
   props: [ 'id' ],
@@ -37,6 +38,14 @@ export default {
             throw new Error(`Recipe with ID ${recipeId} not found`);
           }
         }
+
+        // checkbox status
+    if (this.recipe.ingredients) {
+      this.recipe.ingredients = this.recipe.ingredients.map(ing => ({
+        ...ing,
+        checked: false  
+      }));
+    }
         
       } catch (error) {
         console.error('Error loading recipe:', error);
@@ -55,7 +64,8 @@ export default {
     }
   },
   components: { 
-    Ratingstars
+    Ratingstars,
+    ListComponent
   }
 }
 
@@ -80,6 +90,17 @@ export default {
       :src="recipe.imageUrl"
       :alt="recipe.title"
       class="recipe-image" />
+
+      <ListComponent
+    title="Instructions"
+    :items="recipe.instructions"
+    listTag="ol"
+    variant="instructions"
+  >
+    <template #item="{ item }">
+      <li>{{ item }}</li>
+    </template>
+  </ListComponent>
     </div>
     
     <div class="recipe-content"> <!-- Right part of the page -->
@@ -101,7 +122,7 @@ export default {
       <div class="recipe-details"> <!-- Recipe details with icons -->
         <div class="time-icon">
           <i class="bi bi-clock"></i>
-          <span>{{ recipe.timeInMins }}</span>
+          <span>{{ recipe.timeInMins }} minuter</span>
         </div>
 
         <div class="ingredient-icon">
@@ -109,29 +130,29 @@ export default {
           <span>{{ recipe.ingredients.length }} ingredienser</span> 
         </div>
       </div>
-        
-        <div class="ingredients-block">  <!-- Ingredients section -->
-        <h2>Ingredientser</h2>
-        <ul class="ingredients-list"> 
-            <li v-for="item in recipe.ingredients"
-            :key="item">
-                {{ item }}
-            </li>
-        </ul>
-    </div>
-  </div>
-  </div>
 
-  <div class="instructions-block">  <!-- Instructions section -->
-      <h2>Instructioner</h2>
-      <ol class="instructions-list">
-        <li v-for="step in recipe.instructions"
-        :key="step">
-          {{ step }}
-        </li>
-      </ol>
+      <div class="description">
+        <p>{{ recipe.description }}</p>
+      </div> 
+        
+          <ListComponent
+  title="Ingredients"
+  :items="recipe.ingredients"
+  variant="ingredients"
+>
+  <template #item="{ item }">
+  <li class="ingredient-item">
+    <input type="checkbox" v-model="item.checked" />
+
+    <span :class="{ checked: item.checked }">
+      {{ item.amount }} {{ item.unit }} {{ item.name }}
+    </span>
+  </li>
+</template>
+</ListComponent>
   </div>
     </div>
+      </div>
     
     <Ratingstars 
       :initial-rating="recipe.rating || 0" 
@@ -171,8 +192,8 @@ export default {
 }
 
 .recipe-page-content {
-  max-width: 1100px; /* Limit content width so text doesn’t become too wide */ 
-  margin: 40px auto;
+  max-width: 100%; /* Limit content width so text doesn’t become too wide */ 
+  margin: 0 auto;
   padding: 20px;
   color: #d7d4d4;
 }
@@ -197,26 +218,25 @@ export default {
 
 .card-container {
   display: grid; /* image + text */
-  grid-template-columns: 480px 1fr;
+  grid-template-columns: 600px 1fr;
   gap: 40px;
   align-items: start;
-  margin-bottom: 30px; /* Space below the grid */
+  margin-bottom: 30px;
 }
 
 /* Left side image */
 .recipe-image-box {
   width: 600px;
-  height: 600px;
-  border-radius: 12px;
-  overflow: hidden;
-  transform: translateX(-150px);
-  margin-top: -90px;
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
 }
 
 .recipe-image {
   width: 100%;
-  height: 100%;
+  height: 600px;
   object-fit: cover;
+  border-radius: 12px;
 }
 
 /* Right side content */
@@ -227,7 +247,6 @@ export default {
   font-size: 16px;
   padding-right: 10px;
   padding-left: 20px;
-  margin-top: -90px; /* Align with image */
   color: #444;
 
 }
@@ -295,57 +314,20 @@ export default {
   gap: 8px;
 }
 
+.description {
+  margin: 20px 0;
+  margin-bottom: 40px;
+  font-weight: lighter;
+  font-size: 18px;
+  line-height: 1.6;
+  color: #555;
+}
+
 h2 {
   font-family: "Playfair Display", serif;
   font-size: 28px;
   font-weight: 600;
   margin-top: 40px;
   color: #444;
-}
-
-.ingredients-block {
-  background: #f5f0f8;          
-  padding: 14px 20px 18px;
-  margin-top: 10px;              
-  border-radius: 14px;
-  box-shadow: 0 2px 6px rgba(156, 156, 156, 0.5);
-}
-
-.ingredients-block h2 {
-  margin-top: 0;
-  margin-bottom: 10px;
-}
-
-.ingredients-list {
-  list-style: none;
-  padding: 0;
-  margin: 0;
-}
-
-.ingredients-list li {
-  padding: 14px 0;
-  border-bottom: 1px solid #ddd;
-  font-size: 16px;
-  line-height: 1.6;
-  color: #333;
-}
-
-.ingredients-list li:last-child { /* Remove border from last item */
-  border-bottom: none;
-}
-
-/* Instructions Section */
-.instructions-block {
-  margin-left: -140px; /* Align with image */
-  margin-top: 20px;
-  color: #444;
-}
-
-.instructions-list {
-  padding-top: 10px;
-  padding-left: 15px;
-  line-height: 1.8;
-  font-size: 16px;
-  font-family: "Montserrat", sans-serif;
 }
 </style>
